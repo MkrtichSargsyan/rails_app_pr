c1d41682d88188c054bbd1a27e6253990c188b74
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2020-10-16 04:19:39 +0400

    initial commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,22 @@
+class PostsController < ApplicationController
+  def index; end
+
+  def show
+
+  end
+
+  def new
+ p 'dsf'
+  end
+
+  def create; end
+  
+
+  def edit
+
+  end
+
+  def update; end
+
+  def delete; end
+end

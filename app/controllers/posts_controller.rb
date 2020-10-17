0659abe6555755f06949114883cebd3b13c2b5e0
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2020-10-17 08:58:23 +0400

    Create show action for showing single post by id

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,9 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
-  def show; end
+  def show
+    @post = Post.find(params[:id])
+  end
 
   def new
     @post = Post.new
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
   resources :posts
-  # get 'posts', to: 'posts#index'
 
   root to: 'posts#index'
 end
